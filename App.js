4684bdd0f1d4ca01883e355587ff449f53e374fc
Chang Doh <cwdoh@riotgames.com>
2020-07-27 17:18:23 +0900

    07. Custom component

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+class CounterButton extends React.Component {
   state = {
     counter: 0,
   };
@@ -16,8 +16,18 @@ export default class App extends React.Component {
   
     return (
       <View style={styles.container}>
-          <Text>{this.state.counter}</Text>
-          <Button title={'click me!'} onPress={clickHandler}/>
+        <Text>{this.state.counter}</Text>
+        <Button title={'click me!'} onPress={clickHandler}/>
+      </View>
+    );
+  }
+}
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <CounterButton />
         <StatusBar style="auto" />
       </View>
     );
