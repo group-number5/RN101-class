de0878fdded41741fe15f57e705f88898c3706ce
Chang Doh <cwdoh@riotgames.com>
2020-07-27 17:14:59 +0900

    05. handler: function

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { Button, StyleSheet, View } from 'react-native';
 
 export default function App() {
+  const clickHandler = () => console.log('Clicked!');
+
   return (
     <View style={styles.container}>
-      <Button title={'click me!'} onPress={() => console.log('Clicked!')}/>
+        <Button title={'click me!'} onPress={clickHandler}/>
       <StatusBar style="auto" />
     </View>
   );
