1130f1bb18f1655b42f0ebbe80bd314243e504bd
Chang Doh <cwdoh@riotgames.com>
2020-07-27 20:48:37 +0900

    30. React-Navigation

--- App.js
+++ App.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import * as React from 'react';
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import CityList from './CityList';
 
-// after `expo install react-native-safe-area-context`.
-import { SafeAreaView } from 'react-native-safe-area-context';
+const HomeScreen = () => (
+  <View style={styles.container}>
+    <CityList/>
+    <StatusBar style="auto" />
+  </View>
+);
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <CityList/>
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    );
-  }
-}
+const Stack = createStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
 });
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
