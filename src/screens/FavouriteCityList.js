b851afed55b12e910602158ddcd4d31c823db745
kimkihoon <rlarlgns0515@ajou.ac.kr>
2020-08-03 00:45:01 +0900

    즐겨찾기와 일출일몰 업데이트

---
+++ src/screens/FavouriteCityList.js
@@ -0,0 +1,53 @@
+import React, {useState, useEffect} from 'react';
+import { View, FlatList, StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+export default FavouriteCityList = (props) => {
+    const [cities, setCities] = useState()
+
+    useEffect(()=>{
+        if(cities != props.favouriteCities) setCities(props.favouriteCities)
+    })
+
+    const onPressCity = (item) => {
+        console.log('onPressCity =', item);
+        props.addToFavouriteCities(item)
+        props.navigation.navigate('Detail', {
+        city: item
+        });
+    }
+
+    const renderItem = (city) => {
+        return (
+        <TouchableOpacity style={styles.item} onPress={() => onPressCity(city)}>
+            <Text style={styles.text}>{city}</Text>
+        </TouchableOpacity>
+        );
+    }
+
+    return (
+        <View>
+            <Text>최근 검색목록</Text>
+            <FlatList style={styles.container}
+                        numColumns={3}
+                        renderItem={({ item }) => renderItem(item)}
+                        keyExtractor={item => item}
+                        data={cities}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height : 50
+    },
+    item: {
+        flex: 1,
+        height: 50,
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 14,
+        textAlign: 'center',
+    }
+});
