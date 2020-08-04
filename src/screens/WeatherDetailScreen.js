7ff4fbee1bb6a792fc474ef5ab9d254744b412ba
sanghee <sanghee0610@ajou.ac.kr>
2020-08-05 01:21:54 +0900

    Add Max & min temp and temperature symbol

--- src/screens/WeatherDetailScreen.js
+++ src/screens/WeatherDetailScreen.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { ActivityIndicator, Image, StyleSheet, View, Text } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import openWeatherApi from '../api/OpenWeatherApi';
-import Constants from 'expo-constants';
-import _get from 'lodash.get';
+import React from "react";
+import { ActivityIndicator, Image, StyleSheet, View, Text } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import openWeatherApi from "../api/OpenWeatherApi";
+import Constants from "expo-constants";
+import _get from "lodash.get";
 
 export default class WeatherDetailScreen extends React.Component {
   constructor(props) {
@@ -17,7 +17,8 @@ export default class WeatherDetailScreen extends React.Component {
   componentDidMount() {
     this.setState({ isLoading: true });
 
-    openWeatherApi.fetchWeatherInfoByCityName(this.props.route.params.city)
+    openWeatherApi
+      .fetchWeatherInfoByCityName(this.props.route.params.city)
       .then(info => {
         console.log(info);
         this.setState({
@@ -29,49 +30,49 @@ export default class WeatherDetailScreen extends React.Component {
 
   renderTemperature() {
     const celsius = this.state.main.temp - 273.15;
+    const celsiusMin = this.state.main.temp_min - 273.15;
+    const celsiusMax = this.state.main.temp_max - 273.15;
 
     return (
-      <Text>온도: {celsius.toFixed(1)}</Text>
-    )
-  }
-
-  renderClouds() {
-    const clouds = _get(this.state, ['clouds', 'all'], null);
-
-    const cloudStatus = [
-      '맑음',
-      '구름 조금',
-      '구름 많음',
-      '흐림',
-      '매우 흐림'
-    ];
-
-    const text = (clouds === null) ? '정보 없음' : cloudStatus[Math.max(parseInt(clouds / 20), 4)];
-
-    return (
-      <Text>구름: {text}</Text>
+      <Text>
+        온도: {celsius.toFixed(1)}°C 최저기온: {celsiusMin.toFixed(1)}°C
+        최고기온: {celsiusMax.toFixed(1)}°C{" "}
+      </Text>
     );
   }
 
+  renderClouds() {
+    const clouds = _get(this.state, ["clouds", "all"], null);
+
+    const cloudStatus = ["맑음", "구름 조금", "구름 많음", "흐림", "매우 흐림"];
+
+    const text =
+      clouds === null
+        ? "정보 없음"
+        : cloudStatus[Math.max(parseInt(clouds / 20), 4)];
+
+    return <Text>구름: {text}</Text>;
+  }
+
   renderWind() {
-    const speed = _get(this.state, ['wind', 'speed'], null);
-    const deg = _get(this.state, ['wind', 'deg'], null);
-    
+    const speed = _get(this.state, ["wind", "speed"], null);
+    const deg = _get(this.state, ["wind", "deg"], null);
+
     const arrowStyle = {
-      transform: [
-         { rotate: `${deg}deg`}
-      ],
+      transform: [{ rotate: `${deg}deg` }],
       width: 24,
       height: 24,
     };
 
     return (
       <View style={[styles.inRow, styles.alignItemInCenter]}>
-        <Text>
-          풍속: {speed? `${speed}m/s` : '정보 없음'}
-        </Text>
+        <Text>풍속: {speed ? `${speed}m/s` : "정보 없음"}</Text>
         <View style={[arrowStyle]}>
-          <MaterialCommunityIcons name="arrow-up-circle" size={24} color="black" />
+          <MaterialCommunityIcons
+            name="arrow-up-circle"
+            size={24}
+            color="black"
+          />
         </View>
       </View>
     );
@@ -79,17 +80,16 @@ export default class WeatherDetailScreen extends React.Component {
 
   renderWeatherCondition() {
     // https://openweathermap.org/weather-conditions
-    return this.state.weather.map(({
-      icon,
-      description,
-    }, index) => {
+    return this.state.weather.map(({ icon, description }, index) => {
       return (
         <View style={styles.weatherCondition} key={index}>
-          <Image source={{
-            uri: `http://openweathermap.org/img/wn/${icon}@2x.png`,
-            width: 72,
-            height: 48
-          }} />
+          <Image
+            source={{
+              uri: `http://openweathermap.org/img/wn/${icon}@2x.png`,
+              width: 72,
+              height: 48,
+            }}
+          />
           <Text style={styles.textCondition}>{description}</Text>
         </View>
       );
@@ -97,11 +97,13 @@ export default class WeatherDetailScreen extends React.Component {
   }
 
   renderGoogleMap() {
-    const { 
-      lat, lon
-    } = this.state.coord;
+    const { lat, lon } = this.state.coord;
 
-    const googleApiKey = _get(Constants, ['manifest', 'extra', 'googleApiKey'], null);
+    const googleApiKey = _get(
+      Constants,
+      ["manifest", "extra", "googleApiKey"],
+      null
+    );
 
     if (!googleApiKey) {
       return undefined;
@@ -111,10 +113,11 @@ export default class WeatherDetailScreen extends React.Component {
 
     return (
       <View style={styles.mapContainer}>
-        <Image style={styles.mapImage}
-          resizeMode={'stretch'}
-          resizeMethod={'scale'}
-          source={{ uri: url, }}
+        <Image
+          style={styles.mapImage}
+          resizeMode={"stretch"}
+          resizeMethod={"scale"}
+          source={{ uri: url }}
         />
       </View>
     );
@@ -135,17 +138,15 @@ export default class WeatherDetailScreen extends React.Component {
         <View style={styles.container}>
           <ActivityIndicator size="large" />
         </View>
-      )
+      );
     }
-    
+
     return (
       <View style={styles.container}>
         {this.renderClouds()}
         {this.renderTemperature()}
         {this.renderWind()}
-        <View style={styles.inRow}>
-          {this.renderWeatherCondition()}
-        </View>
+        <View style={styles.inRow}>{this.renderWeatherCondition()}</View>
 
         {this.renderGoogleMap()}
       </View>
@@ -156,35 +157,35 @@ export default class WeatherDetailScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#8888FF',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#8888FF",
+    alignItems: "center",
+    justifyContent: "center",
   },
   inRow: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   alignItemInCenter: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   mapContainer: {
-    width: '90%',
+    width: "90%",
     borderWidth: 1,
-    borderColor: '#2222AA'
+    borderColor: "#2222AA",
   },
   mapImage: {
     aspectRatio: 1,
   },
   weatherCondition: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginBottom: 20,
   },
   textCondition: {
-    color: '#FFF',
+    color: "#FFF",
   },
   rotation: {
     width: 50,
     height: 50,
-    transform: [{ rotate: "5deg" }]
-  }
+    transform: [{ rotate: "5deg" }],
+  },
 });
